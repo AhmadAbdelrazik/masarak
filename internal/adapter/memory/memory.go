d119b8824f6e7eaec837f5af9d4d58008aa1abb6
AhmadAbdelrazik <ahmadabdelrazik159@gmail.com>
2025-03-18 15:11:14 +0200

    Implemented Application struct

--- internal/adapter/memory/memory.go
+++ internal/adapter/memory/memory.go
@@ -7,6 +7,7 @@ import (
 	"github.com/ahmadabdelrazik/linkedout/internal/core/domain/entity"
 	"github.com/ahmadabdelrazik/linkedout/internal/core/domain/job"
 	"github.com/ahmadabdelrazik/linkedout/internal/core/domain/owner"
+	app "github.com/ahmadabdelrazik/linkedout/internal/core/services"
 )
 
 type Memory struct {
@@ -24,5 +25,16 @@ func NewMemory() *Memory {
 		owners:    make([]*owner.Owner, 0),
 		jobs:      make([]*job.Job, 0),
 		companies: make([]*company.Company, 0),
+		authUsers: make([]*entity.AuthUser, 0),
+		tokens:    make(map[[32]byte]string),
+	}
+}
+
+func NewInMemoryRepositories(mem *Memory) *app.Repositories {
+	return &app.Repositories{
+		Companies: NewInMemoryCompanyRepository(mem),
+		Jobs:      NewInMemoryJobRepository(mem),
+		Owner:     NewInMemoryOwnerRepository(mem),
+		AuthUsers: NewInMemoryAuthUserRepository(mem),
 	}
 }
--- internal/core/services/app.go
+++ internal/core/services/app.go
@@ -1,12 +1,43 @@
 package app
 
+import (
+	"github.com/ahmadabdelrazik/linkedout/internal/core/domain/company"
+	"github.com/ahmadabdelrazik/linkedout/internal/core/domain/entity"
+	"github.com/ahmadabdelrazik/linkedout/internal/core/domain/job"
+	"github.com/ahmadabdelrazik/linkedout/internal/core/domain/owner"
+)
+
 type Application struct {
-	Commands Commands
-	Queries  Queries
+	Commands *Commands
+	Queries  *Queries
+}
+
+func NewApplication(repos *Repositories) *Application {
+	commands := &Commands{
+		CreateCompany: NewCreateCompanyHandler(repos.Companies, repos.Owner),
+		CreateJob:     NewCreateJobHandler(repos.Owner, repos.Companies, repos.Jobs),
+		CreateOwner:   NewCreateOwnerHandler(repos.Owner, repos.AuthUsers),
+	}
+	queries := &Queries{}
+
+	return &Application{
+		Commands: commands,
+		Queries:  queries,
+	}
 }
 
 type Commands struct {
+	CreateCompany *CreateCompanyHandler
+	CreateJob     *CreateJobHandler
+	CreateOwner   *CreateOwnerHandler
 }
 
 type Queries struct {
+}
+
+type Repositories struct {
+	Companies company.Repository
+	Jobs      job.Repository
+	Owner     owner.Repository
+	AuthUsers entity.AuthUserRepository
 }
