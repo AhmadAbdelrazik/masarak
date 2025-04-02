5731485ca9e84bd5083f410cad50768f88b2efd9
AhmadAbdelrazik <ahmadabdelrazik159@gmail.com>
2025-04-02 08:01:35 +0200

    Introduced Roles and Permissions

--- internal/core/domain/valueobject/role.go
+++ internal/core/domain/valueobject/role.go
@@ -1,42 +1,102 @@
 package valueobject
 
 import (
-	"strings"
-
 	"github.com/pkg/errors"
 )
 
 type Role struct {
-	role string
+	role        string
+	permissions []string
 }
 
-func (r *Role) Is(role string) bool {
-	return r.role == strings.ToLower(role)
+func (r *Role) HasPermission(permission string) bool {
+	for _, p := range r.permissions {
+		if p == permission {
+			return true
+		}
+	}
+
+	return false
 }
 
-func (r *Role) String() string {
+func (r *Role) Role() string {
 	return r.role
 }
 
 var (
-	roleUser   Role = Role{role: "user"}
-	roleAdmin       = Role{role: "admin"}
-	roleOwner       = Role{role: "owner"}
-	roleTalent      = Role{role: "talent"}
+	roleUser Role = Role{
+		role:        "user",
+		permissions: []string{},
+	}
+	roleFreelancer = Role{
+		role: "freelancer",
+		permissions: []string{
+			"create:freelancer_profile",
+			"read:freelancer_profile",
+			"update:freelancer_profile",
+			"delete:freelancer_profile",
+
+			"create:resume",
+			"read:resume",
+			"update:resume",
+			"delete:resume",
+
+			"create:application",
+			"read:application",
+			"update:application",
+			"delete:application",
+
+			"read:job",
+
+			"read:application_history",
+		},
+	}
+	roleBusinessOwner = Role{
+		role: "business_owner",
+		permissions: []string{
+			"create:business",
+			"read:business",
+			"update:business",
+			"delete:business",
+
+			"create:job",
+			"read:job",
+			"update:job",
+			"delete:job",
+
+			"read:application",
+			"update:application",
+		},
+	}
+	roleBusinessEmployee = Role{
+		role: "business_employee",
+		permissions: []string{
+			"read:business",
+			"update:business",
+
+			"create:job",
+			"read:job",
+			"update:job",
+			"delete:job",
+
+			"read:application",
+			"update:application",
+		},
+	}
 
 	ErrInvalidRole = errors.New("invalid role")
 )
 
 func NewRole(role string) (*Role, error) {
 	switch role {
-	case "admin", "Admin":
-		return &roleAdmin, nil
-	case "owner", "Owner":
-		return &roleOwner, nil
-	case "user", "User":
+	case "owner":
+		return &roleBusinessOwner, nil
+	case "employee":
+		return &roleBusinessEmployee, nil
+	case "user":
 		return &roleUser, nil
-	case "talent", "Talent":
-		return &roleUser, nil
+	case "freelancer":
+		return &roleFreelancer, nil
 	default:
 		return nil, ErrInvalidRole
 	}
